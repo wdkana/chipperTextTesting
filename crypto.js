bbf96b673251995dab42c85b6aca22040fa6a3f5
fadhlurrahmanID <jedacoding@gmail.com>
2022-03-13 02:00:52 +0800

    Fix Errors Cryptography

--- crypto.js
+++ crypto.js
@@ -1,63 +1,68 @@
 const input = require("prompt");
 const colors = require("@colors/colors/safe");
-
-input.message = colors.rainbow("CyptoTest:");
+input.message = colors.rainbow("CryptoTest:");
 input.delimiter = colors.green("->");
 
-const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
+const alfabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const convert = (isi, sandi, action) => {
-  let urutanList = [];
-  let pecahPesan = [...isi];
-  let urutanKeHuruf = [];
-  let lengthAlpha = alphabet.length;
+const convert = (isi, sandi) => {
+    let pecahPesan = [...isi];
+    let urutanList = [];
+    let urutanKeHuruf = [];
 
-  if (sandi > 26) {
-    sandi = (sandi - lengthAlpha) % lengthAlpha;
-  }
+    pecahPesan.map((pesan) => {
+        let urutan = alfabet.indexOf(pesan);
+        const totalUrutan = urutan + parseInt(sandi);
+        if (totalUrutan > alfabet.length) {
+            return urutanList.push(totalUrutan - alfabet.length);
+        } else {
+            return urutanList.push(totalUrutan);
+        }
+    });
 
-  pecahPesan.map((pesan) => {
-    let indexAlpha;
-    let indexPilihan;
+    urutanList.map((list) => {
+        let urutan = list,
+            alphabetLengthFromZero = alfabet.length - 1;
 
-    if (action === "dec") {
-      // dec for decrypt
-      indexAlpha = alphabet.indexOf(pesan) - parseInt(sandi) + 1;
-    } else {
-      // enc for encrypt
-      indexAlpha = parseInt(sandi) + alphabet.indexOf(pesan) + 1;
-    }
+        // jikalau dia lebih dari 25
+        if (list > alphabetLengthFromZero) {
+            console.log("masuk");
 
-    if (indexAlpha > 26) {
-      indexPilihan = indexAlpha - lengthAlpha;
-    } else {
-      indexPilihan = indexAlpha;
-    }
+            const multiply = Math.floor(list / alphabetLengthFromZero); // misal Math.floor(26 / 25) == 1
+            urutan = list - alphabetLengthFromZero * multiply - 1; // (26 - 25 * 1) - 1 == 0
+        }
 
-    urutanList.push(indexPilihan);
-  });
+        /*
+        
+          penjelasan: jikalau lebih dari dari length alphabet maka dia start dari 0
+                      midal jikalau sudah 26 maka hasil nya == 0 / a << alfabetnya
+                      jadi setiap lebih dari 26 > 52 dan seterusnya ... akan dimulai dari 0
 
-  urutanList.map((urutan) => {
-    urutanKeHuruf.push(alphabet[urutan - 1]);
-  });
+        */
 
-  return urutanKeHuruf.join("");
+        return urutanKeHuruf.push(alfabet[urutan]);
+    });
+
+    const encrypt = urutanKeHuruf.join("");
+    return encrypt;
 };
 
 const main = () => {
-  input.start();
-  input.get(["pengirim", "tujuan", "isi", "sandi"], (err, result) => {
-    const { pengirim, tujuan, isi, sandi } = result;
-
-    const encryptText = convert(isi, sandi, "enc");
-    const dencryptText = convert(encryptText, sandi, "dec");
-
-    console.log(
-      `pesan dikirim dari ${pengirim} kepada ${tujuan}, pesan originalnya adalah '${isi}' telah di encrypt menggunakan sandi ${sandi}:\n`
-    );
-    console.log("encrypted message with chipper method: ", encryptText);
-    console.log("dencrypt message with dencrypter method: ", dencryptText);
-  });
+    console.log("running input ...");
+    input.start();
+    input.get(["pengirim", "tujuan", "isi", "sandi"], (err, result) => {
+        if (err) return err;
+        const data = {
+            pengirim: result.pengirim,
+            tujuan: result.tujuan,
+            isi: result.isi,
+            sandi: result.sandi,
+        };
+        const { pengirim, tujuan, isi, sandi } = data;
+        const encryptedText = convert(data.isi, data.sandi);
+        console.log(`pesan dikirim dari ${pengirim} kepada ${tujuan}, pesan originalnya adalah '${isi}' telah di encrypt menggunakan sandi ${sandi}:\n`);
+        console.log("encrypted message with chipper method: \n", encryptedText);
+    });
 };
 
 main();
